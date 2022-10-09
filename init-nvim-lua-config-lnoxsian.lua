fc42d2edb6f086f5036e01c5e2e6173bc99edd54
lnoxsian <97118089+lnoxsian@users.noreply.github.com>
2022-10-09 16:00:05 +0530

    Create init-nvim-lua-config-lnoxsian.lua

---
+++ init-nvim-lua-config-lnoxsian.lua
@@ -0,0 +1,121 @@
+-- Config was built using the following config:
+-- https://github.com/numToStr/dotfiles/tree/master/neovim/.config/nvim/
+local g   = vim.g
+local o   = vim.o
+local opt = vim.opt
+local A   = vim.api
+
+vim.cmd('syntax on')
+-- vim.api.nvim_command('filetype plugin indent on')
+
+o.termguicolors = true
+o.background = 'dark'
+
+-- Do not save when switching buffers
+-- o.hidden = true
+
+-- Decrease update time
+o.timeoutlen = 500
+o.updatetime = 200
+
+-- Number of screen lines to keep above and below the cursor
+o.scrolloff = 3
+
+-- Better editor UI
+o.number = true
+o.numberwidth = 2
+o.relativenumber = true
+o.signcolumn = 'yes'
+o.cursorline = false
+
+-- Better editing experience
+o.expandtab = true
+o.smarttab = true
+o.cindent = true
+o.autoindent = true
+o.wrap = true
+o.textwidth = 300
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = -1 -- If negative, shiftwidth value is used
+o.list = true
+o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂'
+-- o.listchars = 'eol:¬,space:·,lead: ,trail:·,nbsp:◇,tab:→-,extends:▸,precedes:◂,multispace:···⬝,leadmultispace:│   ,'
+-- o.formatoptions = 'qrn1'
+
+-- Makes neovim and host OS clipboard play nicely with each other
+o.clipboard = 'unnamedplus'
+
+-- Case insensitive searching UNLESS /C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+-- Undo and backup options
+o.backup = false
+o.writebackup = false
+o.undofile = true
+o.swapfile = false
+
+-- Remember 50 items in commandline history
+o.history = 50
+
+-- Better buffer splitting
+o.splitright = true
+o.splitbelow = true
+
+-- This is for mousemovement in neovim for gui apps and terms
+opt.mouse = "a"
+
+-- Map <leader> to space
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+require('lualine').setup()
+
+-- COLORSCHEMES
+local ok, _ = pcall(vim.cmd, 'colorscheme carbonfox') -- this is if carbonfox theme is installed in nightfox theme in neovim
+
+-- Highlight the region on yank
+A.nvim_create_autocmd('TextYankPost', {
+    group = num_au,
+    callback = function()
+        vim.highlight.on_yank({ higroup = 'Visual', timeout = 120 })
+    end,
+})
+
+-- KEYBINDINGS
+local function map(m, k, v)
+    vim.keymap.set(m, k, v, { silent = true })
+end
+
+-- Mimic shell movements
+map('i', '<C-E>', '<ESC>A')
+map('i', '<C-A>', '<ESC>I')
+-- Delete a word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- New tab
+keymap.set('n', 'te', ':tabedit')
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+-- Move window
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('', 'sh', '<C-w>h')
+keymap.set('', 'sk', '<C-w>k')
+keymap.set('', 'sj', '<C-w>j')
+keymap.set('', 'sl', '<C-w>l')
+
+-- Resize window
+keymap.set('n', '<C-w><left>', '<C-w><')
+keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
+-- This is for neovim having a shared clipborad
+vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
